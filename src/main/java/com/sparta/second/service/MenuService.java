a108b4bbc5756250b454cb5b4130d4fbe77546a6
doehyoung kim <hkcheksybr@naver.com>
2023-07-18 20:36:15 +0900

    메뉴 Service

---
+++ src/main/java/com/sparta/second/service/MenuService.java
@@ -0,0 +1,69 @@
+package com.sparta.second.service;
+
+import com.sparta.second.dto.MenuListResponseDto;
+import com.sparta.second.dto.MenuRequestDto;
+import com.sparta.second.dto.MenuResponseDto;
+import com.sparta.second.entity.Menu;
+import com.sparta.second.entity.User;
+import com.sparta.second.repository.MenuRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.RejectedExecutionException;
+
+@Service
+@RequiredArgsConstructor
+public class MenuService {
+
+    private final MenuRepository menuRepository;
+
+    // 메뉴 전체 조회
+    public MenuListResponseDto getMenus() {
+        List<Menu> menuList = menuRepository.findAllByOrderByModifiedAtDesc();
+        return new MenuListResponseDto(menuList.stream().map(MenuResponseDto::new).toList());
+    }
+    // 메뉴 상세 조회
+    public Menu findMenu(Long id) {
+        return menuRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("선택한 메뉴가 존재하지 않습니다."));
+    }
+
+    // 메뉴 생성
+    @Transactional
+    public void createMenu(MenuRequestDto menurequestDto) {
+        menuRepository.save(new Menu(menurequestDto));
+    }
+
+    //메뉴 삭제
+    @Transactional
+    public void deleteMenu(Long menu_id, User user) {
+        String id = user.getUserName();
+        Menu menu = findMenu(menu_id);
+
+        if (id.equals(user.getUserName())) {
+            throw new RejectedExecutionException();
+        } else {
+            menuRepository.delete(menu);
+        }
+    }
+
+    // 메뉴 수정
+    @Transactional
+    public MenuResponseDto updateMenu(Long menu_id, MenuRequestDto menurequestDto, User user) {
+        String id = user.getUserName();
+        Menu menu = findMenu(menu_id);
+
+        if (id.equals(user.getUserName())) {
+            throw new RejectedExecutionException();
+        }
+        menu.setMenuCategory(menurequestDto.getMenuCategory());
+        menu.setMenuTitle(menurequestDto.getMenuTitle());
+        menu.setMenuContent(menurequestDto.getContent());
+        menu.setMenuImage(menurequestDto.getMenuImage());
+
+        return new MenuResponseDto(menu);
+    }
+}
