92cae503c1825fea27d6736b7da24f11f11a38b5
alxqkekrroqkfwk <fexxet@icloud.com>
2023-07-18 10:59:34 +0900

    JpaRepository를 상속 받은 UserRepository

---
+++ src/main/java/com/sparta/second/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.sparta.second.repository;
+
+import com.sparta.second.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUserName(String userName);
+}
