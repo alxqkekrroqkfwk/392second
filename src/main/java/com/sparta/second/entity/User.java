468eed4ca9685be695135fe60ec2c04628a4f98a
alxqkekrroqkfwk <fexxet@icloud.com>
2023-07-18 10:57:15 +0900

    User entity 게터,세터 어노테이션 추가 .
    Table 어노테이션 추가해 데이터베이서에 users라는 이름으로 테이블 생성됨.
    
    필드에 컬럼 어노테이션 생성.

---
+++ src/main/java/com/sparta/second/entity/User.java
@@ -0,0 +1,40 @@
+package com.sparta.second.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.net.URL;
+
+@Getter
+@Entity
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,unique = true)
+    private String userName;
+
+    @Column(nullable = false)
+    private String userPassword;
+
+    @Column(nullable = false)
+    private String getUserNick;
+
+    @Column(nullable = false)
+    @Email
+    private String userEmail;
+
+    @Column
+    private String myContent;
+
+    @Column
+    private URL myImage;
+}
