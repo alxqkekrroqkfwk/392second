572cb3cb8e891e4e5fa12dab21c27fdedf4af149
alxqkekrroqkfwk <fexxet@icloud.com>
2023-07-18 10:59:57 +0900

    User 정보를 받아오기 위한 Dto

---
+++ src/main/java/com/sparta/second/requestDto/UserRequestDto.java
@@ -0,0 +1,13 @@
+package com.sparta.second.requestDto;
+
+
+import lombok.Getter;
+
+@Getter
+public class UserRequestDto {
+
+    private String userName;
+    private String userPassword;
+    private String getUserNick;
+    private String userEmail;
+}
