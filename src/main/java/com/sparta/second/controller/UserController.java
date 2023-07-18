d0c5ad30793f61f2342c5046350a5630f871c521
hee0hee <132984807+hee0hee@users.noreply.github.com>
2023-07-18 21:34:39 +0900

    profile update 비밀번호 검증(인코더 활용), 이미지 구현 시도

--- src/main/java/com/sparta/second/controller/UserController.java
+++ src/main/java/com/sparta/second/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.sparta.second.controller;
 
+import com.sparta.second.dto.PasswordRequestDto;
 import com.sparta.second.dto.ProfileUpdateDto;
 import com.sparta.second.security.UserDetailsImpl;
 import com.sparta.second.service.UserService;
@@ -7,10 +8,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @Controller
@@ -26,12 +25,22 @@ public class UserController {
         return userService.detailProfile(userDetails);
     }
 
-    @PostMapping("/profile")
-    public ProfileUpdateDto profileUpdateDto (@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody ProfileUpdateDto profileUpdateDto) {
-        return userService.updateProfile(profileUpdateDto, userDetails);
+    @PutMapping("/profile")
+    public ProfileUpdateDto profileUpdateDto (@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                              @RequestBody ProfileUpdateDto profileUpdateDto,
+                                              @RequestParam(value = "image", required = false) MultipartFile image,
+                                              PasswordRequestDto requestDto) throws Exception {
+
+        return userService.updateProfile(profileUpdateDto, userDetails, requestDto);
+        // msg로 반환하기로 바꾸기!
+        // MsgResponseDto를 ProfileUpdateDto로 감싼다
     }
 
     //프로필업데이트디티오 -> 로그인과 다른
     //User = profile 필요 정보가 같음
+
+    // 1 비밀번호 검증, 인코더 확인 수정하기
+    // 2 MsgResponseDto 반환해주기
+    // 3 회원가입, 로그인 끝나면 이용해서 포스트맨 확인하기
 
 }
--- src/main/java/com/sparta/second/service/UserService.java
+++ src/main/java/com/sparta/second/service/UserService.java
@@ -6,14 +6,17 @@ import com.sparta.second.entity.User;
 import com.sparta.second.repository.UserRepository;
 import com.sparta.second.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public ProfileUpdateDto detailProfile (UserDetailsImpl userDetails) {
         ProfileUpdateDto profileUpdateDto = new ProfileUpdateDto(userDetails.getUser());
@@ -21,23 +24,28 @@ public class UserService {
     }
 
     @Transactional
-    public boolean checkPassword(UserDetailsImpl userDetails, PasswordRequestDto requestDto) {
-        // 수정 페이지로 넘어가기 전 비밀번호 확인
-        System.out.println(requestDto.getPassword());
-        if (!requestDto.getPassword().equals(userDetails.getUser().getUserPassword())) {
+    public ProfileUpdateDto updateProfile (ProfileUpdateDto profileUpdateDto, UserDetailsImpl userDetails, PasswordRequestDto requestDto) throws Exception {
+
+        requestDto.setPassword(passwordEncoder.encode(requestDto.getPassword()));
+        // 비밀번호를 암호화하여 requestDto의 비밀번호 필드에 설정
+
+        if (!passwordEncoder.matches(requestDto.getPassword(), userDetails.getUser().getUserPassword())) {
             throw new IllegalArgumentException("잘못된 비밀번호입니다.");
         }
-        return true;
-    }
+        // 수정 페이지로 넘어가기 전 비밀번호 확인
 
-    @Transactional
-    public ProfileUpdateDto updateProfile (ProfileUpdateDto profileUpdateDto, UserDetailsImpl userDetails) {
-        User user = userDetails.getUser();
-        user.setUserName(profileUpdateDto.getUserName());
-        user.setUserPassword(profileUpdateDto.getUserPassword());
-        user.setUserNick(profileUpdateDto.getUserNick());
-        user.setMyContent(profileUpdateDto.getMyContent());
+        User targetUser = userRepository.findById(userDetails.getUser().getId()).orElseThrow(() -> new Exception ());
+        // targetUser는 User클래스의 객체이다
+        // userRepository에서 userId로 user를 한 개 찾아온다
+        // userId는 현재 로그인 한 Id를 찾아와야 함으로 userDetails를 실행시킨다
+        // 반환타입이 Optional이기 때문에 null 값에 대한 처리를 해주기 위해 orElseThrow(() -> new Exception ())으로 null 값인 경우 Exception을 던진다
+
+        // DB에 있는 유저의 정보를 바꾸기 위해 User를 가져오고 userRepository 안에 id를 찾아와라
+
+        targetUser.updateProfile(profileUpdateDto);
+
         return profileUpdateDto;
+        // 수정완료  MsgResponseDto로 반환해야 함
     }
 
 
