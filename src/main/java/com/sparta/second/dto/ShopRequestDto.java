15aa95ba6f97f16e2f3de2f9bbcd2ff9e1b5bdf4
Hojin37 <hwlsdl37@gmail.com>
2023-07-18 23:26:48 +0900

    가게(Shop) Request&Response Dto

---
+++ src/main/java/com/sparta/second/dto/ShopRequestDto.java
@@ -0,0 +1,15 @@
+package com.sparta.second.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ShopRequestDto {
+
+    private String shopName;
+    private String shopContent;
+    private String userName;
+
+}
