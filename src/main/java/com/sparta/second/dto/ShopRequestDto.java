6780284908e5eacb04f1fb47a5571c948021bd9e
Hojin37 <hwlsdl37@gmail.com>
2023-07-19 14:56:35 +0900

    가게(Shop) RequestDto에 shopImage, category 추가

--- src/main/java/com/sparta/second/dto/ShopRequestDto.java
+++ src/main/java/com/sparta/second/dto/ShopRequestDto.java
@@ -3,13 +3,15 @@ package com.sparta.second.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import java.net.URL;
 
 @Getter
 @Setter
 public class ShopRequestDto {
 
+    private String category;
     private String shopName;
     private String shopContent;
     private String userName;
-
+    private URL shopImage;
 }
