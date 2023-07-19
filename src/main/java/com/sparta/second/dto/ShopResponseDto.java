b0b530c408c3daf54af94027cd7ca713fee9cec8
Hojin37 <hwlsdl37@gmail.com>
2023-07-19 14:56:51 +0900

    가게(Shop) ResponseDto에 shopImage, category 추가

--- src/main/java/com/sparta/second/dto/ShopResponseDto.java
+++ src/main/java/com/sparta/second/dto/ShopResponseDto.java
@@ -3,16 +3,22 @@ package com.sparta.second.dto;
 import com.sparta.second.entity.Shop;
 import lombok.Getter;
 
+import java.net.URL;
+
 @Getter
 public class ShopResponseDto {
 
     private Long shopId;
+    private String category;
     private String shopName;
     private String shopContent;
+    private URL shopImage;
 
     public ShopResponseDto(Shop shop) {
         this.shopId = shop.getShopId();
+        this.category = shop.getCategory();
         this.shopName = shop.getShopName();
         this.shopContent = shop.getShopContent();
+        this.shopImage = shop.getShopImage();
     }
 }
