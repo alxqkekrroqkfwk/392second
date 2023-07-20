3323c9fd9fb7f500b9d15e39b997496de5244938
alxqkekrroqkfwk <fexxet@icloud.com>
2023-07-20 15:00:49 +0900

    Shop 예외처리 및 반환타입 수정

--- src/main/java/com/sparta/second/repository/ShopRepository.java
+++ src/main/java/com/sparta/second/repository/ShopRepository.java
@@ -5,8 +5,12 @@ import com.sparta.second.entity.Shop;
 import com.sparta.second.dto.ShopResponseDto;
 import com.sparta.second.entity.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+
+@Repository
 public interface ShopRepository extends JpaRepository<Shop, Long> {
     List<Shop> findAllByOrderByModifiedAtDesc();
 }
